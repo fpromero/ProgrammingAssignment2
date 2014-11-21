8ad193a24853455a9210b928896828256adaa470
fpromero <fpromero@gmail.com>
2014-11-21 20:39:02 +0100

    caching solve with comments

--- cachematrix.R
+++ cachematrix.R
@@ -9,6 +9,7 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
+  ## assignation function
   set <- function(y) {
     x <<- y
     inv <<- NULL
@@ -26,8 +27,9 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function computes the inverse of the special only if the matrix values have been changed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   inverse <- x$getinverse()
+  ## if the inverse is cached it is not neccesary to execute solve
   if(!is.null(inverse)) {
     message("getting cached data")
     return(inverse)
@@ -37,3 +39,26 @@ cacheSolve <- function(x, ...) {
   x$setinverse(inverse)
   inverse
 }
+
+# TEST CASE
+#> mat<-matrix(c(5, 7, 13, 4, 1, 7, 14, 3, 11), nrow=3, ncol=3)
+#> mat <- makeCacheMatrix(A)
+#> mat$get()
+#[,1] [,2] [,3]
+#[1,]    5    4   14
+#[2,]    7    1    3
+#[3,]   13    7   11
+# Case 1: Without using Cache
+#> cacheSolve(mat)
+#[,1]        [,2]         [,3]
+#[1,] -0.03311258  0.17880795 -0.006622517
+#[2,] -0.12582781 -0.42052980  0.274834437
+#[3,]  0.11920530  0.05629139 -0.076158940
+# Case 2: using Cache
+#> cacheSolve(mat)
+#getting cached data
+#[,1]        [,2]         [,3]
+#[1,] -0.03311258  0.17880795 -0.006622517
+#[2,] -0.12582781 -0.42052980  0.274834437
+#[3,]  0.11920530  0.05629139 -0.076158940
+
